6e611bb025b461210338ecbc58b36fc9ef8005f6
Reiss Jarvis <ReissJarvis@etech.net>
2017-07-21 15:07:58 +0100

    add inital code

---
+++ index.js
@@ -0,0 +1,14 @@
+const connectWithRetry = module.exports = async (promiseToRetry, serverName, retryNumber, timeout) => {
+    try {
+        console.log(`connecting to ${serverName}, attempt: ${retryNumber || 0}`)
+        await new Promise(resolve => setTimeout(resolve, timeout || 0))
+        return await promiseToRetry
+    }
+    catch (err) {
+        if (retryNumber === 5) {
+            return Promise.reject(`Failed after 5 retries to call ${serverName}`)
+        }
+
+        await connectWithRetry(promiseToRetry, serverName,  retryNumber + 1 || 1, Math.pow(2, retryNumber) * 1000)
+    }
+}
